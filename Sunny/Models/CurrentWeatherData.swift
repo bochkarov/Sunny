7d13a376a48a970b588c7e79e3ab17ceb99cc109
Valentyn Bochkarov <bochkarov90@gmail.com>
2020-06-28 12:05:55 +0200

    First Commit

---
+++ Sunny/Models/CurrentWeatherData.swift
@@ -0,0 +1,30 @@
+//
+//  CurrentWeatherData.swift
+//  Sunny
+//
+//  Created by Bochkarov Valentyn on 07/04/2020.
+//  Copyright © 2020 Ivan Akulov. All rights reserved.
+//
+
+import Foundation
+
+struct CurrentWeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+    
+    struct Main: Codable {
+        let temp: Double
+        let feelsLike: Double
+        
+        enum CodingKeys: String, CodingKey {
+            case temp
+            case feelsLike = "feels_like"
+        }
+        
+    }
+    
+    struct Weather: Codable {
+        let id: Int
+    }
+}
---
+++ Sunny/Supporting files/Constans.swift
@@ -0,0 +1,10 @@
+//
+//  Constans.swift
+//  Sunny
+//
+//  Created by Bochkarov Valentyn on 07/04/2020.
+//  Copyright © 2020 Ivan Akulov. All rights reserved.
+//
+
+import Foundation
+ let apiKey = "70402fb735cad5b1c13b9aa4ba2977cf"
